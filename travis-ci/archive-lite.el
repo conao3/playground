5ee5f27a484b0c02e54f3a985f91810f0f202f6a
conao3 <conao3@gmail.com>
2018-12-19 23:56:59 +0900

    add archive-lite.el

---
+++ travis-ci/archive-lite.el
@@ -0,0 +1,15 @@
+(require 'json)
+(setq json-object-type 'hash-table)
+(setq json-key-type    'keyword)
+
+(let* ((current-dir (file-name-directory
+                     (directory-file-name load-file-name)))
+       (write-file  (concat current-dir "archive-lite-sexp.el"))
+       (read-file   (concat current-dir "archive-lite.json"))
+       (contents))
+  (if (and (file-writable-p write-file)
+           (file-readable-p read-file))
+      (with-temp-file write-file
+        (setq contents  (prin1-to-string (json-read-file read-file)))
+        (insert contents))
+    (error "file error")))
