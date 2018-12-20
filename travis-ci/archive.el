39ea6c02871d13d50ad4f1e6b70fe435fade9cf3
conao3 <conao3@gmail.com>
2018-12-20 11:50:45 +0900

    recieve filepath as a command args

--- travis-ci/archive.el
+++ travis-ci/archive.el
@@ -1,18 +1,23 @@
 (require 'json)
-(setq json-object-type 'hash-table)
-(setq json-key-type    'keyword)
 
-(let* ((current-dir (file-name-directory
-                     (directory-file-name load-file-name)))
-       (write-file  (concat current-dir "archive-sexp.el"))
-       (read-file   (concat current-dir "archive.json"))
-       (contents))
-  (if (and (file-writable-p write-file)
-           (file-readable-p read-file))
-      (with-temp-file write-file
-        (setq contents  (prin1-to-string (json-read-file read-file)))
-        (insert contents)
-        (insert "\n"))
-    (error "File open error")))
+(let ((json-object-type 'plist)
+      (json-array-type  'list)
+      (json-key-type    'keyword))
+  (let* ((current-dir (file-name-directory
+                       (directory-file-name load-file-name)))
+         (read-file   (concat current-dir (nth 0 command-line-args-left)))
+         (write-file  (concat current-dir (nth 1 command-line-args-left)))
+         (contents))
+    (princ (format "Process completed!!\nRead file: %s\nWrite file: %s\n"
+                   read-file write-file))
+    (if (and (file-writable-p write-file)
+             (file-readable-p read-file))
+        (progn
+          (with-temp-file write-file
+            (setq contents (prin1-to-string (json-read-file read-file)))
+            (insert contents)
+            (insert "\n"))
+          (princ (format "Process completed!!\nRead file: %s\nWrite file: %s\n"
+                         read-file write-file)))
+      (error "File open error"))))
 
-(princ "Process completed!!")
