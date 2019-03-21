a94b2fa1797884b51feae9a27af959f0d905ddf0
conao3 <conao3@gmail.com>
2019-03-21 16:29:28 +0900

    add package comment

--- clojure/dali/project.clj
+++ clojure/dali/project.clj
@@ -4,7 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 ;; http server
                  [ring "1.7.1"]
+
+                 ;; svg utility
                  [dali "0.7.4"]]
   :main ^:skip-aot conao3.dali.core
   :target-path "target/%s"
