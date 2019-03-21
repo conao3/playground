eeed86a86fac0408f0e66035a52349f3c37a82c9
conao3 <conao3@gmail.com>
2019-03-21 16:25:21 +0900

    add dali document, render function

--- clojure/dali/src/conao3/dali/core.clj
+++ clojure/dali/src/conao3/dali/core.clj
@@ -2,6 +2,15 @@
   (:require [dali.io :as io])
   (:gen-class))
 
+(def document
+ [:dali/page
+  [:circle
+   {:stroke :indigo :stroke-width 4 :fill :darkorange}
+   [30 30] 20]])
+
+(defn render-png []
+  (io/render-png document "/tmp/hello.png"))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
