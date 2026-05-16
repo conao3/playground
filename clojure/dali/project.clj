5be3a9b99d47f10f1a2ecadf98b28b9009ab9bb0
conao3 <conao3@gmail.com>
2026-05-16 03:56:28 +0000

    chore: update clojure dependencies to latest versions

--- clojure/dali/project.clj
+++ clojure/dali/project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.12.5"]
                  ;; http server
-                 [compojure "1.6.1"]
-                 [ring "1.7.1"]
+                 [compojure "1.7.2"]
+                 [ring "1.15.4"]
 
                  ;; svg utility
-                 [dali "0.7.4"]]
+                 [dali "1.0.2"]]
   ;; :main ^:skip-aot conao3.dali.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
